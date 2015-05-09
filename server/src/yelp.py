b400df4b4fa97d6b42c6e0e44689d1ca9946b42c
g-votte <s.g.votte@gmail.com>
2015-05-09 17:41:12 +0900

    implemented yelp2es api

--- server/src/yelp.py
+++ server/src/yelp.py
@@ -1,33 +1,72 @@
+import os
 import rauth
+from data import Data
 
 
 class YelpApiWrapper(object):
-    def __init__(self, consumer_key, consumer_secret, token, token_secret):
+    def __init__(self, consumer_key, consumer_secret, token, token_secret,
+                 radius_filter=500, limit=20):
         self.consumer_key = consumer_key
         self.consumer_secret = consumer_secret
         self.token = token
         self.token_secret = token_secret
 
-    @classmethod
-    def get_search_parameters(cls, latitude, longitude, radius_filter=2000, limit=20):
+        self.radius_filter = radius_filter
+        self.limit = limit
+
+    def get_search_parameters(self, latitude, longitude):
         params = dict()
         params["term"] = "restaurant"
         params["ll"] = "{},{}".format(str(latitude), str(longitude))
-        params["radius_filter"] = str(radius_filter)
-        params["limit"] = str(limit)
+        params["radius_filter"] = str(self.radius_filter)
+        params["limit"] = str(self.limit)
 
         return params
 
-    def yelp_search(self, request_paramters):
+    def request_yelp_search(self, request_parameters):
         session = rauth.OAuth1Session(
             consumer_key=self.consumer_key,
             consumer_secret=self.consumer_secret,
             access_token=self.token,
             access_token_secret=self.token_secret)
 
-        request = session.get("http://api.yelp.com/v2/search", params=request_paramters)
+        request = session.get(
+            'http://api.yelp.com/v2/search',
+            params=request_parameters)
+
+        print request.url
 
         data = request.json()
         session.close()
 
         return data
+
+    def yelp_geo_search_and_elastic_search_put(self, latitude, longitude):
+        param = self.get_search_parameters(latitude, longitude)
+        yelp_businesses = self.request_yelp_search(param)['businesses']
+
+        for business in yelp_businesses:
+            data = Data(
+                instance_id=1,
+                name=business['name'],
+                geo=[
+                    float(business['location']['coordinate']['latitude']),
+                    float(business['location']['coordinate']['longitude'])
+                ],
+                reference_url=business['url'],
+                reference_picture=business['snippet_image_url'],
+                probability=float(business['review_count']) / 10000.0,
+                reason='float(n_comments) / 10000.0',
+                genre='Yelp Restaurant'
+            )
+            data.put()
+
+
+if __name__ == '__main__':
+    yelp = YelpApiWrapper(
+        os.getenv('YELP_CONSUMER_KEY'),
+        os.getenv('YELP_CONSUMER_SECRET'),
+        os.getenv('YELP_TOKEN'),
+        os.getenv('YELP_TOKEN_SECRET'))
+
+    yelp.yelp_geo_search_and_elastic_search_put(42.24, -83.61)
