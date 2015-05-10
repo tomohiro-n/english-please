b7af1fd78c0a9b6a1a8ee12a9fe190f7b7df62a3
Tomohiro Nakamura <naka.tomohiro@gmail.com>
2015-05-11 00:07:17 +0900

    initial commit for querying to elasticsearch from client side

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -3,7 +3,8 @@ var map;
 var locations = {
 	'shibuya': [35.6585, 139.7013],
 	'shinjuku': [35.6938, 139.7036]
-}
+};
+var data;
 
 function initializeMap() {
 	var mapOptions = {
@@ -12,6 +13,22 @@ function initializeMap() {
 	};
 	map = new google.maps.Map($("#map-canvas")[0],
 		mapOptions);
+	setTimeout(function () {
+		data.hits.hits.forEach(function (hit) {
+			var tmp = hit._source.geo;
+			var myLatlng = new google.maps.LatLng(tmp[0], tmp[1]);
+			var marker = new google.maps.Marker({
+				position: myLatlng,
+				map: map,
+				title: "Hello World!"
+			});
+			google.maps.event.addListener(marker, 'click', function () {
+				$("#place-img").html("<img src='" + hit._source.reference_picture + "' height='80' width='80'>");
+				$("#place-desc").text(hit._source.name);
+				$("#place-reason").text(hit._source.reason);
+			});
+		});
+	}, 1000);
 }
 
 function moveLocation(sel) {
@@ -20,3 +37,16 @@ function moveLocation(sel) {
 
 google.maps.event.addDomListener(window, 'load', initializeMap);
 
+var es = new $.es.Client({
+	host: {
+		host: '52.4.196.153',
+		port: '9200'
+	}
+});
+
+es.search({
+	index: 'ep_venues',
+	type: 'venue'
+}, function (error, response) {
+	data = response;
+});
